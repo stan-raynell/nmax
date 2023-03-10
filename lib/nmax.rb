d082f0dda58ba85e2696589dff47e8ba96096e61
stan-raynell <dunn2000@mail.ru>
2023-03-10 16:48:23 +0300

    Комменты

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -6,19 +6,30 @@ module Nmax
   module_function
 
   def reader
+    # Чтение данных из стандартного ввода
     $stdin.tty? ? "" : $stdin.read
   end
 
   def extractor
-    reader.scan(/\d{1,1000}/).map(&:to_i)
-  end
-
-  def argument
-    ARGV[0]
+    # Поиск чисел в прочтённых данных
+    extract = reader
+    extract.empty? ? "No input!" : extract.scan(/\d{1,1000}/).map(&:to_i)
   end
 
   def output
-    extractor.sort.reverse.first(argument.to_i)
+    # Проверка результата поиска и выдача
+    result = extractor
+    if result == "No input!"
+      result
+    elsif ARGV[0].nil?
+      "No argument!"
+    elsif result.empty?
+      "No digits in input!"
+    elsif !Integer(ARGV[0], exception: false)
+      "Argument is not a number!"
+    else
+      result.sort.last(ARGV[0].to_i).reverse
+    end
   end
 
   p output
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -6,9 +6,9 @@ require "stringio"
 RSpec.describe Nmax do
   before do
     $stdin = StringIO.new("joe123uuu77ff99")
-    ARGV << "2"
+    ARGV[0] = "2"
   end
-  it "receives input from STDIN" do
+  it "reads input from STDIN" do
     expect(Nmax.reader).to eq("joe123uuu77ff99")
   end
 
@@ -16,12 +16,28 @@ RSpec.describe Nmax do
     expect(Nmax.extractor).to eq([123, 77, 99])
   end
 
-  it "gets a command line argument" do
-    expect(Nmax.argument).to eq("2")
+  it "provides proper sorted output" do
+    expect(Nmax.output).to eq([123, 99])
   end
 
-  it "provides 2 sorted numbers" do
-    expect(Nmax.output).to eq([123, 99])
+  it "detects no input" do
+    $stdin = StringIO.new("")
+    expect(Nmax.output).to eq("No input!")
+  end
+
+  it "detects no argument" do
+    ARGV.clear
+    expect(Nmax.output).to eq("No argument!")
+  end
+
+  it "detects input with no digits" do
+    $stdin = StringIO.new("ksjdbkabjfvkafgvkagk")
+    expect(Nmax.output).to eq("No digits in input!")
+  end
+
+  it "detects wrong type of argument" do
+    ARGV[0] = "b"
+    expect(Nmax.output).to eq("Argument is not a number!")
   end
 
   after do
